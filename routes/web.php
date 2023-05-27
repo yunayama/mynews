79ab52bbfcfb0bac4a2a900a0c1c23a7b152abf6
山森祐菜 <y.yamamori013fmeets.com>
2023-05-27 22:55:06 +0900

    4-12課題

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\Admin\NewsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\SelfProfileController;
 
@@ -21,4 +21,7 @@ Route::get('/', function () {
 Route::controller(SelfProfileController::class)->prefix('admin')->name('profile.')->group(function(){
     Route::get('profile/create', 'add')->name('add');
     Route::get('profile/edit', 'edit')->name('edit');
+});
+Route::controller(NewsController::class)->prefix('admin')->name('news.')->group(function(){
+    Route::get('news/create', 'add')->name('add');
 });
